454b96dc8ce5f673666e90ef2256ad5f7b2f3075
arinata098 <arinata098@gmail.com>
2024-03-01 11:46:51 +0800

    part 4

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,28 @@
       <h1>Pinia Todos</h1>
     </header>
 
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetail :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
+import TaskDetail from './components/TaskDetail.vue'
+import { useTaskStore } from './stores/TaskStore'
+
   export default {
-    
+    components: {
+      TaskDetail
+    },
+    setup () {
+      const taskStore = useTaskStore()
+
+      return { taskStore }
+    }
   }
 </script>
