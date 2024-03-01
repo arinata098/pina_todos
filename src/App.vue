c80db56bcd310f949b4d2ed8d4de9dbbefba259e
arinata098 <arinata098@gmail.com>
2024-03-01 11:14:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="Pinia Logo">
+      <h1>Pinia Todos</h1>
+    </header>
+
+  </main>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
